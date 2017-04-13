f4b3e04091d64989f3c0792a7be6247e239be3a8
Unknown <camila.batista@cwi.com.br>
2017-04-13 17:43:06 -0300

    Revert "add categoria"
    This reverts commit e4b6202180e6c7bd0a9dee7af7d8bcb99b00b3ba.

--- CavaleiroZodiaco/Armadura.java
+++ CavaleiroZodiaco/Armadura.java
@@ -1,13 +1,12 @@
 public class Armadura{
     private String constelacao;
-    private Categoria categoria;
-    //Se o valor do enum for declarado desta forma, o valor é instanciado quando a variavel é declarada
+    
+
+   
     
     public Armadura(){}
-    public Armadura(String constelacao, Categoria categoria){
-        this.constelacao = constelacao;    
-        //this.categoria = Categoria.OURO;
-        this.categoria = categoria;
+    public Armadura(String constelacao){
+        this.constelacao = constelacao;
     }
     
     
