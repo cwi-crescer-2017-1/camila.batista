a17647a0c4b21ec991df9281a7d77927e4b4589d
Unknown <camila.batista@cwi.com.br>
2017-04-13 16:21:36 -0300

    Classe Armadura

---
+++ CavaleiroZodiaco/Armadura.java
@@ -0,0 +1,14 @@
+public class Armadura{
+    private String constelacao;
+    
+
+   
+    
+    public Armadura(){}
+    public Armadura(String constelacao){
+        this.constelacao = constelacao;
+    }
+    
+    
+    
+}
