39255a49e669ddea15674be920dbd5f47bede2a7
Unknown <camila.batista@cwi.com.br>
2017-04-17 15:13:42 -0300

    refactory do método iniciar

--- CavaleiroZodiaco/Batalha.java
+++ CavaleiroZodiaco/Batalha.java
@@ -1,6 +1,7 @@
 public class Batalha{
     private Saint saint1;
     private Saint saint2;
+	private double dano = 10.;
     
     public Batalha(Saint saint1, Saint saint2){
         this.saint1 = saint1;
@@ -11,12 +12,10 @@ public class Batalha{
 		int valor1 = this.saint1.getArmadura().getCategoria().getValor();
 		int valor2 = this.saint2.getArmadura().getCategoria().getValor();
 	
-       if(valor1 > valor2){
-           this.saint2.perderVida(10);
-        }else if(valor1 == valor2){
-        	this.saint2.perderVida(10);
-		}else{
-            this.saint1.perderVida(10);
+       if(valor1 >= valor2){
+           this.saint2.perderVida(dano);
+        }else{
+            this.saint1.perderVida(dano);
         }
     }
 }
