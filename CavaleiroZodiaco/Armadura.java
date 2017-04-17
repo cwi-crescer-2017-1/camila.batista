af1fd64a4817d882da835b04d03864afa1dbe754
Unknown <camila.batista@cwi.com.br>
2017-04-17 16:29:07 -0300

    feat(armadura): adicionar método getConstelacao

--- CavaleiroZodiaco/Armadura.java
+++ CavaleiroZodiaco/Armadura.java
@@ -10,6 +10,8 @@ public class Armadura{
     public Categoria getCategoria(){
         return this.categoria;
     }
-    
+    public String getConstelacao(){
+		return this.constelacao;
+	}	
     
 }
