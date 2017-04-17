fc2998c0729a4982ab22389c5828fcc0959a1ac1
Unknown <camila.batista@cwi.com.br>
2017-04-16 23:00:03 -0300

    feat(class): adicionar batalha

---
+++ CavaleiroZodiaco/Batalha.java
@@ -0,0 +1,17 @@
+public class Batalha{
+    private Saint saint1;
+    private Saint saint2;
+    
+    public Batalha(Saint saint1, Saint saint2){
+        this.saint1 = saint1;
+        this.saint2 = saint2;
+    }
+    
+    public void iniciar(){
+       if((this.saint1.getArmadura().getCategoria().getValor()) > (this.saint2.getArmadura().getCategoria().getValor())){
+           this.saint2.perderVida(10);
+        }else{
+            this.saint1.perderVida(10);
+        }
+    }
+}
