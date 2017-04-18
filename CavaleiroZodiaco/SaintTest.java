6851717556ea481185455d1ec1251095107e94e2
Unknown <camila.batista@cwi.com.br>
2017-04-17 21:10:50 -0300

    test(Saint): verificar se quando a vida menor que 1 status morto

--- CavaleiroZodiaco/SaintTest.java
+++ CavaleiroZodiaco/SaintTest.java
@@ -32,37 +32,37 @@ public class SaintTest
         assertEquals(Genero.NAO_INFORMADO, s13.getGenero());
     }
 
-	@Test
-	public void possivelAlterarGenero() throws Exception{
-	    Saint s33 = new Saint ("Pudim", new Armadura("Touro", Categoria.PRATA));
-		s33.setGenero(Genero.FEMININO);
-		assertEquals(Genero.FEMININO, s33.getGenero());
-	}    
+    @Test
+    public void possivelAlterarGenero() throws Exception{
+        Saint s33 = new Saint ("Pudim", new Armadura("Touro", Categoria.PRATA));
+        s33.setGenero(Genero.FEMININO);
+        assertEquals(Genero.FEMININO, s33.getGenero());
+    }    
 
     @Test
     public void aoCriarSaintVidaCem() throws Exception{
-		Saint s = new Saint ("Alguem", new Armadura("Touro", Categoria.PRATA));
+        Saint s = new Saint ("Alguem", new Armadura("Touro", Categoria.PRATA));
         assertEquals(100.0, s.getVida(), 0);        
     }
    
     @Test
     public void iniciarStatusVivo() throws Exception{
-		Saint s = new Saint ("Alguem", new Armadura("Touro", Categoria.PRATA));
+        Saint s = new Saint ("Alguem", new Armadura("Touro", Categoria.PRATA));
         assertEquals(Status.VIVO, s.getStatus());
     }
 
-	@Test
-	public void danoComValor10() throws Exception{
-		Saint s = new Saint ("Alguem", new Armadura("Touro", Categoria.PRATA));
-		s.perderVida(10.);
-		assertEquals(90, s.getVida(), 0.001);   
-	
-	}   
+    @Test
+    public void danoComValor10() throws Exception{
+        Saint s = new Saint ("Alguem", new Armadura("Touro", Categoria.PRATA));
+        s.perderVida(10.);
+        assertEquals(90, s.getVida(), 0.001);   
+    
+    }   
 
     @Test
     public void perderCemVida() throws Exception{
        Saint s = new Saint ("Alguem", new Armadura("Touro", Categoria.PRATA));
-		vida = s.getVida();
+        vida = s.getVida();
         s.perderVida(100.0);
         assertEquals((vida - 100.0), s.getVida(), 0);
     }
@@ -70,32 +70,39 @@ public class SaintTest
     @Test
     public void perderVidaNegativa() throws Exception{
         Saint s = new Saint ("Alguem", new Armadura("Touro", Categoria.PRATA));
-		vida = s.getVida();
+        vida = s.getVida();
         s.perderVida(-100.0);
-        assertEquals((vida + 100.0), s.getVida(), 0);
+        assertEquals(s.getVida(), s.getVida(), 0);
     }
 
-	@Test
-	public void iniciarSaintCom5SentidosDespertos() throws Exception{
-		BronzeSaint s = new BronzeSaint ("Alguem", new Armadura("Touro", Categoria.BRONZE));
-		assertEquals(5, s.getQtdSentidosDespertos());   
-	}
+    @Test
+    public void iniciarSaintCom5SentidosDespertos() throws Exception{
+        BronzeSaint s = new BronzeSaint ("Alguem", new Armadura("Touro", Categoria.BRONZE));
+        assertEquals(5, s.getQtdSentidosDespertos());   
+    }
 
-	@Test
-	public void saintPrata6Sentidos() throws Exception{
-		PrataSaint s = new PrataSaint ("Alguem", new Armadura("Touro", Categoria.PRATA));
-		assertEquals(6, s.getQtdSentidosDespertos());
-	}
+    @Test
+    public void saintPrata6Sentidos() throws Exception{
+        PrataSaint s = new PrataSaint ("Alguem", new Armadura("Touro", Categoria.PRATA));
+        assertEquals(6, s.getQtdSentidosDespertos());
+    }
 
-	@Test
-	public void saintOuro7Sentidos() throws Exception{
-		OuroSaint s = new OuroSaint ("Alguem", new Armadura("Touro", Categoria.PRATA));
-		assertEquals(7, s.getQtdSentidosDespertos());
-	}
+    @Test
+    public void saintOuro7Sentidos() throws Exception{
+        OuroSaint s = new OuroSaint ("Alguem", new Armadura("Touro", Categoria.PRATA));
+        assertEquals(7, s.getQtdSentidosDespertos());
+    }
 
-	@Test(expected = Exception.class)
-	public void constelacaoInvalidaDeOuroDeveLancarErro() throws Exception{
+    @Test(expected = Exception.class)
+    public void constelacaoInvalidaDeOuroDeveLancarErro() throws Exception{
         new OuroSaint("Bernardo", new Armadura("Café", Categoria.OURO));
 
-	}
+    }
+    
+    @Test
+    public void vidaMenorQue1StatusMorto() throws Exception{
+        PrataSaint s = new PrataSaint ("Alguem", new Armadura("Touro", Categoria.PRATA));
+        s.perderVida(100.);
+        assertEquals(Status.MORTO, s.getStatus());
+    }
 }
