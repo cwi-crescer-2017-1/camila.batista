e92566e9eb9580d268796c87a825d68c29195115
Unknown <camila.batista@cwi.com.br>
2017-04-19 15:04:07 -0300

    feat(listaSaint): adicionar classe e testes referentes

--- CDZ/ListaSaint.java
+++ CDZ/ListaSaint.java
@@ -1,14 +1,29 @@
 import java.util.ArrayList;
 public class ListaSaint{
-    private ArrayList<Saint> saints = new ArrayList<>();
+    private ArrayList<Saint> saints = new ArrayList<Saint>();
     
-    public ListaSaint(){}
-    
-    public void adicionaSaint(Saint saint){
+    public void adicionarSaint(Saint saint){
         this.saints.add(saint);
     }
+
+    public Saint get(int indice){
+        return this.saints.get(indice);
+    }
     
-    public ArrayList<Saint> getListaSaint(){
+    public ArrayList<Saint> todos(){
         return this.saints;
+    }
+    
+    public void remover(Saint saint){
+        this.saints.remove(saint);
+    }
+    
+    public Saint buscaPorNome(String nome){
+        for(Saint saint : this.saints){
+            if(saint.getNome().equals(nome)){
+                return saint;
+            }
+        }
+        return null;
     }
 }
