33d3153256d1a425da67b9bb416653076d484312
Unknown <camila.batista@cwi.com.br>
2017-04-19 03:12:56 -0300

    feat(prataSaint): novo tipo de saint

---
+++ CDZ/PrataSaint.java
@@ -0,0 +1,7 @@
+public class PrataSaint extends Saint{
+    
+    public PrataSaint(String nome, Armadura armadura){
+        super(nome, armadura);
+        this.qtdSentidosDespertos = 6;
+    }
+}
