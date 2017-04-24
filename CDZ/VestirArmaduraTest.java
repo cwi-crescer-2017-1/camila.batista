d468cc34340e82a2fd9180fb05c6311c05848667
Unknown <camila.batista@cwi.com.br>
2017-04-24 15:17:02 -0300

    feat(VestirArmaduraTest): adicionar testes de validação de movimentos

---
+++ CDZ/VestirArmaduraTest.java
@@ -0,0 +1,36 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VestirArmaduraTest
+{
+
+    @Test
+    public void vestirArmadura(){
+        Saint june = new BronzeSaint("June", "Camaleao");
+        Movimento mov = new VestirArmadura(june);
+        
+        mov.executar();
+        assertTrue(june.isVestida());
+    }
+    
+    @Test
+    public void naoVestirArmadura(){
+        Saint june = new BronzeSaint("June", "Camaleao");
+        Movimento mov = new VestirArmadura(june);
+        
+        mov.executar();
+        assertFalse(june.isVestida());
+    }
+    
+    @Test(expected = NullPointerException.class)
+    public void vestirArmaduraComSaintNull() throws Exception{
+        Saint june = null;
+        Movimento mov = new VestirArmadura(june);
+        
+        mov.executar();
+    }
+}
