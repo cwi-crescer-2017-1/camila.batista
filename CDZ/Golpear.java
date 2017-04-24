43eea5c6d1a00c495843c926b3e269bee3bd7221
Unknown <camila.batista@cwi.com.br>
2017-04-24 15:32:08 -0300

    fix(golpear): corrigir construtor
    retirar validação de saint do contrutor e adicionar no método executar

--- CDZ/Golpear.java
+++ CDZ/Golpear.java
@@ -1,12 +1,18 @@
 public class Golpear implements Movimento{
+
+    private Saint golpeado, golpeador;
     
     public Golpear(Saint golpeador, Saint golpeado){
+        this.golpeado = golpeado;
+        this.golpeador = golpeador;
+    }
+    
+    public void executar(){
         if(golpeador.isVestida()){
             golpeado.perderVida((golpeador.getProximoGolpe().getFatorDano()) * (1 + golpeador.getArmadura().getCategoria().getValor()));
         }else{
             golpeado.perderVida(golpeador.getProximoGolpe().getFatorDano());
         }
+
     }
-    
-    public void executar(){}
 }
