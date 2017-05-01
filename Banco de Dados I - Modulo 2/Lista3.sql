953c72e56e101c0d55e899bd8628e3c959640fe8
Unknown <camila.batista@cwi.com.br>
2017-05-01 10:35:49 -0300

    feat(Lista3): adicionar ranking de departamentos

--- Banco de Dados I - Modulo 2/Lista3.sql
+++ Banco de Dados I - Modulo 2/Lista3.sql
@@ -3,3 +3,5 @@
 --Exercício 1
 SELECT IDEmpregado, NomeEmpregado FROM Empregado WHERE YEAR(DataAdmissao) LIKE 1980;
 
+--Exercício 2
+SELECT NomeDepartamento, COUNT(IDEmpregado) AS QuantidadeEmpregados FROM Departamento, Empregado WHERE Departamento.IDDepartamento = Empregado.IDDepartamento GROUP BY NomeDepartamento, Departamento.IDDepartamento;
