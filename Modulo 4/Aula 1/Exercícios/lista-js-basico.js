e48b7321b2c18953db8a398bd5c3466b6b3d4644
Unknown <camila.batista@cwi.com.br>
2017-05-11 18:22:34 -0300

    feat(aula1): primeiros exercicio

---
+++ Modulo 4/Aula 1/Exercícios/lista-js-basico.js
@@ -0,0 +1,26 @@
+//exercicio 1
+function daisyGame(a){
+  if(a % 2 == 0){
+    console.log("Love me not");
+  }else{
+    console.log("Love me");
+  }
+}
+
+console.log(daisyGame(1));
+console.log(daisyGame(2));
+console.log(daisyGame(3));
+console.log(daisyGame(4));
+console.log(daisyGame(5));
+
+
+//Exercício 2
+
+//Exercício 3
+// var instrutor = ['bernardo', 'nunes', 'fabrício', 'ben-hur', 'carlos'];
+// // function imprime (instrutor,
+// function(instrutor){
+//   for(var a in instrutor){
+//   //  console.log("Olá querido instrutor: " + a);
+//   }
+// }
