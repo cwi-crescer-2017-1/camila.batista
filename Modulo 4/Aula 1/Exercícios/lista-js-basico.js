95409a60dba82dc32a261863cea00b4b4e508cc4
Unknown <camila.batista@cwi.com.br>
2017-05-11 23:14:32 -0300

    feat(aula1): adicionado exercício de maiorTexto e imprime

--- Modulo 4/Aula 1/Exercícios/lista-js-basico.js
+++ Modulo 4/Aula 1/Exercícios/lista-js-basico.js
@@ -6,21 +6,34 @@ function daisyGame(a){
     console.log("Love me");
   }
 }
-
-console.log(daisyGame(1));
-console.log(daisyGame(2));
-console.log(daisyGame(3));
-console.log(daisyGame(4));
-console.log(daisyGame(5));
+//
+// console.log(daisyGame(1));
+// console.log(daisyGame(2));
+// console.log(daisyGame(3));
+// console.log(daisyGame(4));
+// console.log(daisyGame(5));
 
 
 //Exercício 2
+function maiorTexto(array){
+  var a = "";
+  for(var i = 0; i < array.length; i++){
+    if(array[i].length > a.length){
+      a = array[i];
+    }
+  }
+  return a;
+}
+
+// var array = ["oi", "ssss", "vvf0", "lalala"];
+// console.log(maiorTexto(array));
 
 //Exercício 3
-// var instrutor = ['bernardo', 'nunes', 'fabrício', 'ben-hur', 'carlos'];
-// // function imprime (instrutor,
-// function(instrutor){
-//   for(var a in instrutor){
-//   //  console.log("Olá querido instrutor: " + a);
-//   }
-// }
+function imprime(array, funcao){
+  if(typeof funcao != "function"){
+    return;
+  }
+  for(var a = 0; a < array.length; a++){
+    funcao(array[a]);
+  }
+}
