420dbe3e20c9e5411c620e6597d05c2930e372db
Unknown <camila.batista@cwi.com.br>
2017-05-17 17:34:51 -0300

    feat(aula1): adicionar exemplo usando ngRepeat e filter

--- Modulo 5/Aula 1/controller.js
+++ Modulo 5/Aula 1/controller.js
@@ -5,3 +5,16 @@ oi.controller('controller1', function($scope, $rootScope){
   //rootScope é o scope padrão colocado pelo ng-app
     $scope.texto = 'Heil Rainha!';
 });
+
+
+oi.controller('exemplo2', function($scope){
+  let pokemons = [
+    {nome: 'pikachu', tipo: 'elétrico'},
+    {nome: 'charmander', tipo: 'fogo'},
+    {nome: 'pokemon 1', tipo: 'agua'},
+    {nome: 'pokemon 2', tipo: 'terra'},
+    {nome: 'pokemon 3', tipo: 'default'},
+    {nome: 'pokemon 4', tipo: 'ar'}
+  ];
+  $scope.pokemons = pokemons;
+});
