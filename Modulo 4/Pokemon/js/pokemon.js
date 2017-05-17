ac94ee2fd5aa50f584624622f9e6169164b197d1
Unknown <camila.batista@cwi.com.br>
2017-05-17 14:47:03 -0300

    fix: alterar váriavel que estava forma de escopo

--- Modulo 4/Pokemon/js/pokemon.js
+++ Modulo 4/Pokemon/js/pokemon.js
@@ -1,14 +1,9 @@
 document.addEventListener('DOMContentLoaded', function(){
-  let numPokemon = document.getElementById('numPok').value;
-  let url = 'http://pokeapi.co/api/v2/pokemon/'+ numPokemon;
   let buttonPesquisar = document.getElementById('buttonPesquisar');
-
   buttonPesquisar.onclick = function() {
+	let url = `http://pokeapi.co/api/v2/pokemon/${document.getElementById("numPok").value}/`;
     fetch(url).then(response => response.json())
     .then(json => {
-
-      //ESSA COISA NÃO TA FUNCIONANDO
-      //Uncaught (in promise) TypeError: Cannot read property 'front_default' of undefined at fetch.then.then.json
 
       let imgPok = document.createElement('img');
       imgPok.src = json.sprites.front_default;
@@ -23,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function(){
       json.types.forEach((e) => {
         let liPok = document.createElement('li');
         liPok.innerHTML = e.type.name;
-        listaPok.appendChild(e);
+        listaPok.append(e);
       });
 
     })
