6fa4956fc624344eff479963c87c63bf12101ed0
Unknown <camila.batista@cwi.com.br>
2017-05-18 00:41:04 -0300

    feat(pokemon): adicionar appends

--- Modulo 4/Pokemon/js/pokemon.js
+++ Modulo 4/Pokemon/js/pokemon.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
   let buttonPesquisar = document.getElementById('buttonPesquisar');
+
   buttonPesquisar.onclick = function() {
-	let url = `http://pokeapi.co/api/v2/pokemon/${document.getElementById("numPok").value}/`;
+	let url = `http://pokeapi.co/api/v2/pokemon/${document.getElementById('numPok').value}/`;
     fetch(url).then(response => response.json())
     .then(json => {
 
-      let imgPok = document.createElement('img');
-      imgPok.src = json.sprites.front_default;
+      let divPok = document.getElementById('divPokemon')
 
       let nomePoK = document.createElement('h2');
       nomePoK.innerText = json.name;
+
+
+      let imgPok = document.createElement('img');
+      imgPok.src = json.sprites.front_default;
 
       let idPok = document.createElement('h3');
       idPok.innerText = json.id;
@@ -18,9 +22,17 @@ document.addEventListener('DOMContentLoaded', function(){
       json.types.forEach((e) => {
         let liPok = document.createElement('li');
         liPok.innerHTML = e.type.name;
-        listaPok.append(e);
+        listaPok.append(liPok);
       });
 
+      divPok.appendChild(nomePoK);
+      divPok.appendChild(imgPok);
+      divPok.appendChild(idPok);
+      divPok.appendChild(listaPok);
     })
   }
 });
+// 
+// document.getElementById('labelPesquisar').addEventListener('clicou', function functionName(event) {
+//     console.log(event);
+// });
