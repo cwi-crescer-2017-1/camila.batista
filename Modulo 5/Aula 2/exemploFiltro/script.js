6c28ac97ffbf565ddad522719b11d6270c6c5075
Unknown <camila.batista@cwi.com.br>
2017-05-18 21:52:42 -0300

    feat(aula2): implementar exercicio 1

--- Modulo 5/Aula 2/exemploFiltro/script.js
+++ Modulo 5/Aula 2/exemploFiltro/script.js
@@ -1,6 +1,16 @@
 var a = angular.module('modulo', []);
 
-a.filter('m', function(){
+function array(arrayInstrutor){
+  var novo = [];
+  angular.forEach(arrayInstrutor, function(instrutor){
+    angular.forEach(instrutor.aula, function(aula){
+      novo.push({instrutor: instrutor.nome, aulaNome: aula.nome, numAula: aula.numero});
+    });
+  });
+  return novo;
+}
+
+a.filter('exercicio2', function(){
   return function(nome){
     return nome.replace(/(nunes)/i, '$ $1 $');
   }
@@ -8,26 +18,7 @@ a.filter('m', function(){
 
 
 a.controller('mascada', function($scope, $filter){
-  $scope.instrutores = [{
-    nome: 'Bernardo',
-    aula: [{
-        numero: 1,
-        nome: 'OO'
-      },
-      {
-        numero: 4,
-        nome: 'Javascript'
-      }
-    ]
-  },
-  {
-    nome: 'Nunes',
-    aula: [{
-      numero: 2,
-      nome: 'Banco de Dados I'
-    }]
-  },
-  {
+  var instrutores = [{
     nome: 'Pedro (PHP)',
     aula: [{
       numero: 3,
@@ -40,6 +31,27 @@ a.controller('mascada', function($scope, $filter){
       numero: 5,
       nome: 'AngularJS'
     }]
+  },
+  {
+    nome: 'Bernardo',
+    aula: [{
+      numero: 1,
+      nome: 'OO'
+    },
+    {
+      numero: 4,
+      nome: 'Javascript'
+    }]
+  },
+  {
+    nome: 'Nunes',
+    aula: [{
+      numero: 2,
+      nome: 'Banco de Dados I'
+    }]
   }];
 
+  $scope.instrutores = instrutores;
+
+  $scope.crescer = array($scope.instrutores);
 });
