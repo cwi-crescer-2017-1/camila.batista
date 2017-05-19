8773bccaaf00ecf5e84c24bc1a714980709e240d
Unknown <camila.batista@cwi.com.br>
2017-05-19 17:54:09 -0300

    feat(aula3): adicionar exercicio de validação formularios

---
+++ Modulo 5/Aula 3/exercicioAula.js
@@ -0,0 +1,27 @@
+var app = angular.module('oi', []);
+
+app.controller('contro', function ($scope) {
+  $scope.instrutores = [{
+    nome: 'Bernardo',
+    sobrenome: 'Rezende',
+    idade: 30,
+    email: 'bernardo@cwi.com.br',
+    jaDeuAula: true,
+    aula: 'OO'
+  }];
+
+  $scope.aulas = [
+    'OO',
+    'HTML e CSS',
+    'Javascript',
+    'AngularJS',
+    'Banco de Dados I'
+  ];
+
+  $scope.cadastrar = function(){
+
+    $scope.instrutores.push(angular.copy($scope.novoInstrutor));
+
+    $scope.novoInstrutor = {};
+  }
+});
