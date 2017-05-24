0a1fab8efecece6b2a52824ee893fd5aa9b9af37
Unknown <camila.batista@cwi.com.br>
2017-05-24 17:36:21 -0300

    feat(aula1): adicionar exemplos

---
+++ Modulo 6/Aula 1/demo 1/demo1/demo1/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    public class Pessoa
+    {
+        public const double PI = 3.14;
+
+        public readonly double PIreadonly = 3.14;
+
+        public Pessoa()
+        {
+            PIreadonly = 3.143;
+        }    
+
+        public string nome { get; set; }
+
+        public int? id { get; set; }
+
+        public DateTime dataNascimento { get; set; }
+
+    }
+}
---
+++ Modulo 6/Aula 1/demo 1/demo1/demo1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+
+            Pessoa p1 = new Pessoa();
+            p1.id = null;
+            p1.dataNascimento = new DateTime(1882, 10, 03);
+            p1.nome = $"Alguem {p1.id}";
+
+            if (p1.id == null)
+            {
+                Console.WriteLine(p1.nome);
+                Console.ReadLine();
+            }
+            var p2 = new Pessoa();
+
+
+            Console.WriteLine("Peso");
+            var peso = Console.ReadLine();
+            Console.WriteLine("Altura");
+            var altura = Console.ReadLine();
+
+
+            var calculo = new CalculoIMC(double.Parse(peso), double.Parse(altura));
+
+            Console.WriteLine(calculo.CalcularIMC());
+            Console.ReadKey();
+
+            List<int> arrayList = new List<int>();
+
+            bool entrada = true;
+            while(entrada)
+            {
+                Console.WriteLine("Digite um valor");
+                var lida = Console.ReadLine();
+
+                if(lida == "exit")
+                {
+                    break;
+                }
+ 
+                arrayList.Add(int.Parse(lida));    
+            }
+
+            foreach(var e in arrayList)
+            {
+                Console.WriteLine(e);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
