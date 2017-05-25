53db80fec8267437f9c8a95c6645710307bf39fc
Unknown <camila.batista@cwi.com.br>
2017-05-25 11:35:38 -0300

    feat(folhaPagamento): adicionar classe desconto

---
+++ Modulo 6/Aula 1/CamilaBatista/FolhaPagamento/Desconto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolhaPagamento
+{
+    public class Desconto
+    {
+        public double Aliquota { get; private set; }
+        public double Valor { get; private set; }
+
+        public Desconto(double aliquota, double valor)
+        {
+            Aliquota = aliquota;
+            Valor = valor;
+        }
+    }
+}
