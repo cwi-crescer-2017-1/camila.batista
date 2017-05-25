57662a55ebe782c11afba8c8ca411e08fe042b18
Unknown <camila.batista@cwi.com.br>
2017-05-25 11:54:59 -0300

    fix(modulo 5): corrigir caminhos do routeProvider

--- Modulo 5/Crud2/principal/principal.js
+++ Modulo 5/Crud2/principal/principal.js
@@ -3,19 +3,15 @@ var a = angular.module('rotas', ['ngRoute']);
 
 a.config(function ($routeProvider) {
   $routeProvider
-    .when('/index', {
-      controller: 'principalController',
-      templateUrl: 'index.html'
-    })
     .when('/aula', {
       controller: 'aulaController',
       templateUrl: 'aula/aula.html'
     })
     .when('/instrutor', {
-      controller: 'intrutorController',
+      controller: 'instrutorController',
       templateUrl: 'instrutor/instrutor.html'
     })
-    .otherwise({redirecTo: '/index'});
+    .otherwise({redirecTo: '/index.html'});
 });
 
 a.controller('principalController', function($scope){
