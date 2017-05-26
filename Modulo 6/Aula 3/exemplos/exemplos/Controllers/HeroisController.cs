45c4a8cc56837254422f434eb3be9b3cb74791a0
camila.batista <camila.batista@cwi.com.br>
2017-05-26 16:51:04 -0300

    feat(aula3): adicionar exercicio

--- Modulo 6/Aula 3/exemplos/exemplos/Controllers/HeroisController.cs
+++ Modulo 6/Aula 3/exemplos/exemplos/Controllers/HeroisController.cs
@@ -10,30 +10,43 @@ namespace exemplos.Controllers
 {
     public class HeroisController : ApiController
     {
+        public static List<Heroi> heroiTemporario = new List<Heroi>();
+
+        private static int contator = 1;
+        private static object objetoLock = new object();
 
         public IEnumerable<Heroi> Get(int? id = null)
         {
-            var herois = new List<Heroi>()
-            {
-                new Heroi {Id = 1, Nome = "Goku", Poder = new Poder() {Nome = "Genki Dama", Dano = 8000 } },
-                new Heroi {Id = 2, Nome = "Luffy", Poder = new Poder() {Nome = "Genki Dama", Dano = 8200 } },
-                new Heroi {Id = 3, Nome = "Ryu", Poder = new Poder() {Nome = "Genki Dama", Dano = 9000 } },
-                new Heroi {Id = 4, Nome = "Madara", Poder = new Poder() {Nome = "Genki Dama", Dano = 212000 } },
-                new Heroi {Id = 5, Nome = "Tom Bombadil", Poder = new Poder() {Nome = "cantar", Dano = 12000 } }
-            };
+            //var herois = new List<Heroi>()
+            //{
+            //    new Heroi {Id = 1, Nome = "Goku", Poder = new Poder() {Nome = "Genki Dama", Dano = 8000 } },
+            //    new Heroi {Id = 2, Nome = "Luffy", Poder = new Poder() {Nome = "Genki Dama", Dano = 8200 } },
+            //    new Heroi {Id = 3, Nome = "Ryu", Poder = new Poder() {Nome = "Genki Dama", Dano = 9000 } },
+            //    new Heroi {Id = 4, Nome = "Madara", Poder = new Poder() {Nome = "Genki Dama", Dano = 212000 } },
+            //    new Heroi {Id = 5, Nome = "Tom Bombadil", Poder = new Poder() {Nome = "cantar", Dano = 12000 } }
+            //};
 
-            return herois.Where(x => (id == null || x.Id == id));
+            return heroiTemporario.Where(x => (id == null || x.Id == id));
         }
 
         public IHttpActionResult Post(Heroi heroi)
         {
-            if (heroi.Id == 0)
+            if (heroi == null)
             {
 
-                return Ok();
-            }
-            else{
                 return BadRequest();
+            }
+            else
+            {
+                //thread safe
+                lock (objetoLock)
+                {
+                    heroiTemporario.Add(heroi);
+                    heroi.Id = contator++;
+                }
+
+                return Ok(heroi);
+
             }
         }
     }
