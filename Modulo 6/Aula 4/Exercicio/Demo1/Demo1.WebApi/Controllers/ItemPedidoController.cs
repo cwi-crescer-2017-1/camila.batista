3c17d1e2451722433f97b94bc6a69e99c280e404
Unknown <camila.batista@cwi.com.br>
2017-05-29 23:18:07 -0300

    feat(aula4): adicionar itemPedidoController

---
+++ Modulo 6/Aula 4/Exercicio/Demo1/Demo1.WebApi/Controllers/ItemPedidoController.cs
@@ -0,0 +1,56 @@
+﻿using Demo1.Dominio.Entidades;
+using Demo1.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Demo1.WebApi.Controllers
+{
+    public class ItemPedidoController : ApiController
+    {
+        ItemPedidoRepositorio _itemPedidoRepositorio = new ItemPedidoRepositorio();
+
+        public IHttpActionResult Post(ItemPedido itemPedido)
+        {
+            var mensagens = new List<string>();
+            if (!itemPedido.Validar(out mensagens))
+            {
+                return BadRequest(string.Join(".", mensagens.ToArray()));
+            }
+
+            _itemPedidoRepositorio.Criar(itemPedido);
+            return Ok(itemPedido);
+        }
+
+        public IHttpActionResult Put(ItemPedido itemPedido)
+        {
+            var mensagens = new List<string>();
+            if(!itemPedido.Validar(out mensagens))
+            {
+                return BadRequest(string.Join(".", mensagens.ToArray()));
+            }
+
+            _itemPedidoRepositorio.Alterar(itemPedido);
+            return Ok(itemPedido);
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_itemPedidoRepositorio.Listar());
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(_itemPedidoRepositorio.Obter(id));
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            _itemPedidoRepositorio.Excluir(id);
+            return Ok();
+        }
+    }
+}
