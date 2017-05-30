be44afa0fd30f32b35560f658d4b33ac40974cfd
Unknown <camila.batista@cwi.com.br>
2017-05-29 21:30:19 -0300

    feat(aula4): adicionar repositorio Produto

---
+++ Modulo 6/Aula 4/Exercicio/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
@@ -0,0 +1,86 @@
+﻿using Demo1.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    public class ProdutoRepositorio
+    {
+        string stringConexao = "Server=13.65.101.67;User Id=camila.batista; Password=123456; Database=aluno19db";
+
+        public void Alterar(Produto produto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Criar(Produto produto)
+        {
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = @"INSERT INTO Produto (Nome, Preco, Estoque) VALUES (@nome, @preco, @estoque)";
+
+                    comando.Parameters.AddWithValue("@nome", produto.Nome);
+                    comando.Parameters.AddWithValue("@preco", produto.Preco);
+                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
+
+                    comando.ExecuteNonQuery();
+                }
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT @@IDENTITY";
+
+                    var result = (decimal)comando.ExecuteScalar();
+                    produto.Id = (int)result;
+                }
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Produto> Listar()
+        {
+            var produtos = new List<Produto>();
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = @"SELECT Id, Nome, Preco, Estoque FROM Produto";
+
+                    var dataReader = comando.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        var produto = new Produto();
+
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+
+                        produtos.Add(produto);
+                    }
+                }
+            }
+            return produtos;
+        }
+
+        public Produto Obter(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
