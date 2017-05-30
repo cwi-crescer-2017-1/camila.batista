4cdc7587f156deaca3bcbab1390f2beea54abde3
camila.batista <camila.batista@cwi.com.br>
2017-05-30 17:46:19 -0300

    feat(aula5): adicionar livrosController

---
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Api/Controllers/LivrosController.cs
@@ -0,0 +1,22 @@
+﻿using EditoraCrescer.Infraesturtura;
+using EditoraCrescer.Infraesturtura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace EditoraCrescer.Api.Controllers
+{
+    public class LivrosController : ApiController
+    {
+        private LivroRepositorio _livroRepositorio = new LivroRepositorio();
+        public IHttpActionResult Get()
+        {
+            var livros = _livroRepositorio.Obter();
+
+            return Ok(livros);
+        }
+    }
+}
