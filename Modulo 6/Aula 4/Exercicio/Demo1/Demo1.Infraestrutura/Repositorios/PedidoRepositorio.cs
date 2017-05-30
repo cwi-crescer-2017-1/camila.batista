775fbe4fa224bb4d235615f4f1a22e86de289e85
Unknown <camila.batista@cwi.com.br>
2017-05-29 21:29:57 -0300

    feat(aula4): adicionar repositorio Pedido

---
+++ Modulo 6/Aula 4/Exercicio/Demo1/Demo1.Infraestrutura/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo1.Dominio.Entidades;
+using System.Data.SqlClient;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    public class PedidoRepositorio : IPedidoRepositorio
+    {
+        string stringConexao = "Server=13.65.101.67;User Id=camila.batista; Password=123456; Database=aluno19db";
+
+        public void Alterar(Pedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Criar(Pedido pedido)
+        {
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = @"INSERT INTO Pedido (NomeCliente, Itens) VALUES (@nomeCliente, @itens)";
+
+                    comando.Parameters.AddWithValue("@nomeCliente", pedido.NomeCliente);
+                    comando.Parameters.AddWithValue("@itens", pedido.Itens);
+
+                    comando.ExecuteNonQuery();
+                }
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "SELECT @@IDENTITY";
+
+                    var result = (decimal)comando.ExecuteScalar();
+                    pedido.Id = (int)result;
+                }
+            }
+
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pedido> Listar()
+        {
+            var pedidos = new List<Pedido>();
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = @"SELECT Id, NomeCliente, Itens FROM Pedido";
+                    var dataReader = comando.ExecuteReader();
+
+                    while (dataReader.Read())
+                    {
+                        var pedido = new Pedido();
+
+                        pedido.Id = (int)dataReader["Id"];
+                        pedido.NomeCliente = (string)dataReader["NomeCliente"];
+                        pedido.Itens = (List<ItemPedido>)dataReader["Itens"];
+
+                        pedidos.Add(pedido);
+                    }
+                }
+            }
+
+            return pedidos;
+        }
+
+        public Pedido Obter(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
