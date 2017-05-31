95dc9dbf702e0acfc571424b03e4a54d189aac3b
camila.batista <camila.batista@cwi.com.br>
2017-05-31 18:25:46 -0300

    feat(aula5): adicionar rotas,  metodos de get e dispose

--- Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Api/Controllers/LivrosController.cs
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Api/Controllers/LivrosController.cs
@@ -6,31 +6,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Mvc;
 
 namespace EditoraCrescer.Api.Controllers
 {
+    [RoutePrefix("api/Livro")]
     public class LivrosController : ApiController
     {
         private LivrosRepositorio _livroRepositorio = new LivrosRepositorio();
 
-        public IHttpActionResult Get()
+        [Route("")]
+        [HttpGet]
+        public IHttpActionResult Obter()
         {
             var livros = _livroRepositorio.Obter();
 
             return Ok(livros);
         }
 
+        [Route("{isbn:int}")]
+        [HttpGet]
+        public IHttpActionResult ObterPorId(int isbn)
+        {
+            var livros = _livroRepositorio.ObterPorId(isbn);
+            return Ok(livros);
+        }
+
+        [Route("{genero}")]
+        [HttpGet]
+        public IHttpActionResult ObterPorGenero(string genero)
+        {
+            var livros = _livroRepositorio.ObterPorGenero(genero);
+            return Ok(livros);
+        }
+
+        [Route("")]
         public IHttpActionResult Post(Livro livro)
         {
             _livroRepositorio.Criar(livro);
             return Ok();
         }
 
-        public IHttpActionResult Delete(int id)
+        [HttpDelete]
+        [Route("{Isbn:int}")]
+        public IHttpActionResult Delete(int Isbn)
         {
-            _livroRepositorio.Remover(id);
+            _livroRepositorio.Remover(Isbn);
             return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _livroRepositorio.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
