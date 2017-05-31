cb34b2f301b336d7f7d7d7856dade3547f24a1bc
Unknown <camila.batista@cwi.com.br>
2017-05-31 04:33:47 -0300

    feat(aula5): adicionar revisor e revisorMap

---
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Mappings/RevisorMap.cs
@@ -0,0 +1,19 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using System.Data.Entity.ModelConfiguration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Mappings
+{
+    public class RevisorMap : EntityTypeConfiguration<Revisor>
+    {
+        public RevisorMap()
+        {
+            ToTable("Revisores");
+            Property(p => p.Nome).HasMaxLength(300);    
+        }
+    }
+}
