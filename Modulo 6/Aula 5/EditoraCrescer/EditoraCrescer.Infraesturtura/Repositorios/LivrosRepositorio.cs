9b6ebdead295154e3c0bade0a70231bd0002a5bf
camila.batista <camila.batista@cwi.com.br>
2017-05-31 18:27:29 -0300

    feat(aula5): metodos de get por id, genero e dispose

--- Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/LivrosRepositorio.cs
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/LivrosRepositorio.cs
@@ -7,13 +7,23 @@ using System.Threading.Tasks;
 
 namespace EditoraCrescer.Infraesturtura.Repositorios
 {
-    public class LivrosRepositorio
+    public class LivrosRepositorio : IDisposable
     {
         private Contexto contexto = new Contexto();
 
         public List<Livro> Obter()
         {
             return contexto.Livros.ToList();
+        }
+
+        public List<Livro> ObterPorId(int isbn)
+        {
+            return contexto.Livros.Where(x => x.Isbn == isbn).ToList(); 
+        }
+
+        public List<Livro> ObterPorGenero(string genero)
+        {
+            return contexto.Livros.Where(x => x.Genero.Contains(genero)).ToList();
         }
 
         public void Criar(Livro livro)
@@ -26,7 +36,12 @@ namespace EditoraCrescer.Infraesturtura.Repositorios
         {
             var livro = contexto.Livros.FirstOrDefault(x => x.Isbn == id);
             contexto.Livros.Remove(livro);
+            contexto.SaveChanges();
         }
 
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
     }
 }
