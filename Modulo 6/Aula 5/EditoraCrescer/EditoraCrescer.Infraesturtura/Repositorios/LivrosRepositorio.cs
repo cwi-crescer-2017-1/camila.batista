3554399a9dfdcea7fcb8dbd5f801f12f9f257627
Unknown <camila.batista@cwi.com.br>
2017-05-31 04:52:30 -0300

    feat(aula5): adicionar metodos obter, criar e remover nos repositorios

---
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/LivrosRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Repositorios
+{
+    public class LivrosRepositorio
+    {
+        private Contexto contexto = new Contexto();
+
+        public List<Livro> Obter()
+        {
+            return contexto.Livros.ToList();
+        }
+
+        public void Criar(Livro livro)
+        {
+            contexto.Livros.Add(livro);
+            contexto.SaveChanges();
+        }
+
+        public void Remover(int id)
+        {
+            var livro = contexto.Livros.FirstOrDefault(x => x.Isbn == id);
+            contexto.Livros.Remove(livro);
+        }
+
+    }
+}
