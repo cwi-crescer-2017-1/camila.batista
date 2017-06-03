1232eba2933064f72707560cd6c1ed221c835dcf
Unknown <camila.batista@cwi.com.br>
2017-06-03 15:55:48 -0300

    feat(aula5): adicionar rota para lancamentos

--- Modulo 6/Aula 5/EditoraCrescer_Front/Services/livroService.js
+++ Modulo 6/Aula 5/EditoraCrescer_Front/Services/livroService.js
@@ -3,16 +3,25 @@ modulo.factory('livroService', function($http){
 
   return {
     listar: Obter,
-    listarPorId: ObterPorId,
+    lancamento: ObterLancamentos,
+    detalhes: ObterPorId,
     criar: Criar
+  }
+
+  function Obter(){
+    return $http.get(url + '/api/Livro');
   }
 
   function Obter(skip){
     return $http.get(url + '/api/Livro/' + skip);
+  }       
+
+  function ObterLancamentos(){
+    return $http.get(url + '/api/Livro/Lancamento');
   }
 
   function ObterPorId(isbn){
-    return $http.get(url + 'api/Livros' + isbn);
+    return $http.get(url + '/api/Livro/Descricao' + isbn);
   }
 
   function Criar(livro){
