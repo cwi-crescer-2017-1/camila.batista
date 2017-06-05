7a49712337ab0af14d80000405595e16a87cb146
Unknown <camila.batista@cwi.com.br>
2017-06-05 10:27:59 -0300

    feat(aula5): adicionar usuarioRepositorio e usuarioMap

---
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Mappings/UsuarioMap.cs
@@ -0,0 +1,25 @@
+﻿using EditoraCrescer.Infraesturtura.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Mappings
+{
+    public class UsuarioMap : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioMap()
+        {
+
+            HasMany(x => x.Permissoes).WithMany().Map(x =>
+            {
+                x.MapLeftKey("IdUsuario");
+                x.MapRightKey("IdPermissao");
+                x.ToTable("UsuarioPermissao");
+            });
+           
+        }
+    }
+}
---
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Entidades
+{
+    public class UsuarioRepositorio
+    {
+        private Contexto contexto = new Contexto();
+
+        public UsuarioRepositorio()
+        {
+
+        }
+
+        public void Criar(Usuario usuario)
+        {
+            if (usuario.Validar())
+            {
+                contexto.Usuarios.Add(usuario);
+                contexto.SaveChanges();
+            }
+        }
+
+        public void Alterar(Usuario usuario)
+        {
+            if (usuario.Validar())
+            {
+                contexto.Entry(usuario).State = EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+        public void Excluir(int id)
+        {
+            var us = contexto.Usuarios.FirstOrDefault(x => x.Id == id);
+            contexto.Usuarios.Remove(us);
+            contexto.SaveChanges();
+        }
+
+        public object Listar()
+        {
+            return contexto.Usuarios
+                            .Select(usuario => new
+                            {
+                                Id = usuario.Id,
+                                Nome = usuario.Nome,
+                                Email = usuario.Email,
+                                Permissoes = usuario.Permissoes
+                            }).ToList();
+        }
+
+        public Usuario Obter(string email)
+        {
+            return contexto.Usuarios.Where(u => u.Email == email).Include(u => u.Permissoes).FirstOrDefault(usuario => usuario.Email == email);
+        }
+    }
+}
