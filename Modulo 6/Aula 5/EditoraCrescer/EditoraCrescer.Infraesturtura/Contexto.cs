ce3dda92829cda29a2247b482d3ab65fcf0cabd3
Unknown <camila.batista@cwi.com.br>
2017-06-05 10:25:29 -0300

    feat(aula5): adicionar usuario no contexto

--- Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Contexto.cs
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Contexto.cs
@@ -19,11 +19,15 @@ namespace EditoraCrescer.Infraesturtura
 
         public DbSet<Revisor> Revisores { get; set; }
 
+        public DbSet<Usuario> Usuarios { get; set; }
+
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Configurations.Add(new AutorMap());
             modelBuilder.Configurations.Add(new LivroMap());
             modelBuilder.Configurations.Add(new RevisorMap());
+            modelBuilder.Configurations.Add(new UsuarioMap());
         }
     }
 }
