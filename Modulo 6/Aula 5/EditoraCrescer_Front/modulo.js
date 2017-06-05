1e878d74877b16b6ea214dbe60dcc50659e01d03
Unknown <camila.batista@cwi.com.br>
2017-06-05 13:27:45 -0300

    feat(aula5): adicionar modulo de auth

--- Modulo 6/Aula 5/EditoraCrescer_Front/modulo.js
+++ Modulo 6/Aula 5/EditoraCrescer_Front/modulo.js
@@ -1,1 +1,14 @@
-var modulo = angular.module('editora', ['ngRoute']);
+var modulo = angular.module('editora', ['ngRoute', 'auth']);
+
+modulo.constant('authConfig', {
+    urlUsuario: 'http://localhost:8081/api/acessos/usuario',
+
+    // Obrigatória - URL da aplicação que possui o formulário de login
+    urlLogin: '/login',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGIN com sucesso
+    urlPrivado: '/privado',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGOUT
+    urlLogout: '/home'
+});
