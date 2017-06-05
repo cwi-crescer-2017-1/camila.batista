dc320b4ec26d2b83257c7ad589d2aa399a8f6d05
Unknown <camila.batista@cwi.com.br>
2017-06-05 10:26:57 -0300

    feat(aula5): adicionar classe usuario e permissao

---
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Entidades/Permissao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditoraCrescer.Infraesturtura.Entidades
+{
+        public class Permissao
+        {
+            public string Nome { get; set; }
+            public int Id { get; set; }
+
+        public Permissao()
+        { }
+
+        public Permissao(string nome)
+            {
+                Nome = nome;
+            }
+        }
+    }
