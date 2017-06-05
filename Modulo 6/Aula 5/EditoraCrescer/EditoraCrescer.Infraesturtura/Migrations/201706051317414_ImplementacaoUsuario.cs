c32936dc8a1bf3db20c8b356c19dd8d25a6c0b01
Unknown <camila.batista@cwi.com.br>
2017-06-05 10:28:41 -0300

    feat(aula5): adicionar migration implementacaoUsuario

---
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Infraesturtura/Migrations/201706051317414_ImplementacaoUsuario.cs
@@ -0,0 +1,56 @@
+namespace EditoraCrescer.Infraesturtura.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ImplementacaoUsuario : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Usuarios",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                        Email = c.String(),
+                        Senha = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Permissaos",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nome = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.UsuarioPermissao",
+                c => new
+                    {
+                        IdUsuario = c.Int(nullable: false),
+                        IdPermissao = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.IdUsuario, t.IdPermissao })
+                .ForeignKey("dbo.Usuarios", t => t.IdUsuario, cascadeDelete: true)
+                .ForeignKey("dbo.Permissaos", t => t.IdPermissao, cascadeDelete: true)
+                .Index(t => t.IdUsuario)
+                .Index(t => t.IdPermissao);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.UsuarioPermissao", "IdPermissao", "dbo.Permissaos");
+            DropForeignKey("dbo.UsuarioPermissao", "IdUsuario", "dbo.Usuarios");
+            DropIndex("dbo.UsuarioPermissao", new[] { "IdPermissao" });
+            DropIndex("dbo.UsuarioPermissao", new[] { "IdUsuario" });
+            DropTable("dbo.UsuarioPermissao");
+            DropTable("dbo.Permissaos");
+            DropTable("dbo.Usuarios");
+        }
+    }
+}
