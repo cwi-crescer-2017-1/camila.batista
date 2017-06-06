503784b1cba82f2bd3e46cde1d18f0a1f5389372
camila.batista <camila.batista@cwi.com.br>
2017-06-06 17:13:35 -0300

    feat(projetoVeiculo): adicionar atributos na entidade pedido

--- Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.Dominio/Entidades/Pedido.cs
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.Dominio/Entidades/Pedido.cs
@@ -12,9 +12,21 @@ namespace Veiculos.Dominio.Entidades
 
         public decimal ValorTotal { get; set; }
 
-        public DateTime DataInicial { get; set; }
+        public DateTime DataInicial { get; private set; }
 
-        public DateTime DataFinal { get; set; }
+        public DateTime DataPrevista { get; set; }
+
+        public DateTime DataFinal { get; private set; }
+
+        public Cliente cliente { get; private set; }
+
+        public Funcionario funcionario { get; private set; }
+
+        public Adicional adicionais { get; private set; }
+
+        public Produto produto { get; private set; }
+
+        public Pacote pacote { get; private set; }
 
         public Pedido() { }
     }
