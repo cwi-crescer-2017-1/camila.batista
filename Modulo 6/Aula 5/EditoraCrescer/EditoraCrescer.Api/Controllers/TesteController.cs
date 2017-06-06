f1ef3902e515e6eceeb7158d05590811b0259417
camila.batista <camila.batista@cwi.com.br>
2017-06-06 13:19:54 -0300

    feat(aula5): modulo auth

---
+++ Modulo 6/Aula 5/EditoraCrescer/EditoraCrescer.Api/Controllers/TesteController.cs
@@ -0,0 +1,20 @@
+﻿using EditoraCrescer.Api.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.Api.Controllers
+{
+
+    [MeuAtributoAutorizacao(Roles = "Publicador")]
+    public class TesteController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
