5a15ebdaa2c01dcc617ff0f038d598188867c070
camila.batista <camila.batista@cwi.com.br>
2017-06-06 15:43:24 -0300

    feat(projetoVeiculo): adicionar mapping de cliente e funcionario

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Mapping/ClienteMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veiculos.Dominio.Entidades;
+
+namespace Veiculos.Infraestrutura.Mapping
+{
+    public class ClienteMap : EntityTypeConfiguration<Cliente>
+    {
+        public ClienteMap()
+        {
+            ToTable("Cliente");
+            HasKey(x => x.Id);
+
+            Property(p => p.Nome).HasMaxLength(300); 
+            Property(x => x.Endereco).HasMaxLength(500);
+            Property(x => x.CPF).HasMaxLength(20);
+
+        }
+    }
+}
---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Mapping/FuncionarioMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veiculos.Dominio.Entidades;
+
+namespace Veiculos.Infraestrutura.Mapping
+{
+    public class FuncionarioMap : EntityTypeConfiguration<Funcionario>
+    {
+        public FuncionarioMap()
+        {
+            ToTable("Funcionario");
+            HasKey(x => x.Id);
+
+            Property(p => p.Nome).HasMaxLength(300);
+
+        }
+    }
+}
