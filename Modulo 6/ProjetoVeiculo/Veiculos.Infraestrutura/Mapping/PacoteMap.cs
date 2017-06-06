d756762be2e191072ba130aaf78cf8444cbe42e8
camila.batista <camila.batista@cwi.com.br>
2017-06-06 15:59:05 -0300

    feat(projetoVeiculo): adicionar mapping em pacote e produto

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Mapping/PacoteMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veiculos.Dominio.Entidades;
+
+namespace Veiculos.Infraestrutura.Mapping
+{
+    public class PacoteMap : EntityTypeConfiguration<Pacote>
+    {
+        public PacoteMap()
+        {
+            ToTable("Pacote");
+            HasKey(x => x.Id);
+
+            Property(x => x.Nome).HasMaxLength(300);
+
+            Property(x => x.Nome).IsRequired();
+            Property(x => x.ValorUnitario).IsRequired();
+        }
+    }
+}
---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Mapping/ProdutoMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veiculos.Dominio.Entidades;
+
+namespace Veiculos.Infraestrutura.Mapping
+{
+    public class ProdutoMap : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoMap()
+        {
+            ToTable("Produto");
+            HasKey(x => x.Id);
+
+            Property(x => x.Nome).HasMaxLength(300);
+            Property(x => x.ValorUnitario).IsRequired();
+            Property(x => x.Nome).IsRequired();
+        }
+    }
+}
