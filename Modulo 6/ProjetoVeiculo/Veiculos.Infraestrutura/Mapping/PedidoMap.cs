c06c303351a153b42193e8ca20b283da0553d079
camila.batista <camila.batista@cwi.com.br>
2017-06-06 16:42:49 -0300

    feat(projetoVeiculo): adicionar mapping pedido e adicional

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Mapping/PedidoMap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veiculos.Dominio.Entidades;
+
+namespace Veiculos.Infraestrutura.Mapping
+{
+    public class PedidoMap : EntityTypeConfiguration<Pedido>
+    {
+        public PedidoMap()
+        {
+            ToTable("Pedido");
+            HasKey(x => x.Id);
+
+
+            HasRequired(x => x.cliente)
+                .WithMany()
+                .HasForeignKey(x => x.cliente.Id);
+
+            HasRequired(x => x.funcionario)
+                .WithMany()
+                .HasForeignKey(x => x.funcionario.Id);
+
+            HasRequired(x => x.adicionais)
+                .WithMany()
+                .HasForeignKey(x => x.adicionais.Id);
+
+            HasRequired(x => x.produto)
+                .WithMany()
+                .HasForeignKey(x => x.produto.Id);
+
+            HasRequired(x => x.pacote)
+                .WithMany()
+                .HasForeignKey(x => x.pacote.Id);
+        }
+
+    }
+}
