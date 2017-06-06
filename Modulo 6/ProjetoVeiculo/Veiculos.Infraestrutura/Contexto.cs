a6b027ede2eeb1651840407a7eac6d7f9335adae
camila.batista <camila.batista@cwi.com.br>
2017-06-06 16:21:15 -0300

    feat(projetoVeiculo): adicionar contexto

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Contexto.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veiculos.Dominio.Entidades;
+using Veiculos.Infraestrutura.Mapping;
+
+namespace Veiculos.Infraestrutura
+{
+    public class Contexto : DbContext
+    {
+        public Contexto() : base(){ }
+        
+        public DbSet<Adicional> Adicionais { get; set; }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        public DbSet<Pacote> Pacotes { get; set; }
+
+        public DbSet<Pedido> Pedidos { get; set; }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AdicionalMap());
+            modelBuilder.Configurations.Add(new ClienteMap());
+            modelBuilder.Configurations.Add(new FuncionarioMap());
+            modelBuilder.Configurations.Add(new PacoteMap());
+            modelBuilder.Configurations.Add(new PedidoMap());
+            modelBuilder.Configurations.Add(new ProdutoMap());
+        }
+
+    }
+}
