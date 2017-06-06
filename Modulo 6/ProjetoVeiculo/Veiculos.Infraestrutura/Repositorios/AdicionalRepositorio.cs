a94caa2ccc378e093670f5d5f9a892583240861e
camila.batista <camila.batista@cwi.com.br>
2017-06-06 17:36:19 -0300

    feat(projetoVeiculo): adicionar adicional repositorio

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Repositorios/AdicionalRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veiculos.Infraestrutura.Repositorios
+{
+    public class AdicionalRepositorio : IDisposable
+    {
+        private Contexto contexto = new Contexto();
+
+
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+
+        public object Obter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ObterPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
