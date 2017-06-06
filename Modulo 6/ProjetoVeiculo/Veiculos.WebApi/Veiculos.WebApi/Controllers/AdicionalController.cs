cea88a152c02d8914ffd6c09d92bd820c1c07bcb
camila.batista <camila.batista@cwi.com.br>
2017-06-06 17:44:58 -0300

    feat(projetoVeiculo): adicionar adicional controller

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.WebApi/Controllers/AdicionalController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Veiculos.Infraestrutura.Repositorios;
+
+namespace Veiculos.WebApi.Controllers
+{
+    [RoutePrefix("api/adicional")]
+    public class AdicionalController : ApiController
+    {
+        readonly AdicionalRepositorio _adicionalRepositorio;
+
+        public AdicionalController()
+        {
+            _adicionalRepositorio = new AdicionalRepositorio();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Obter()
+        {
+            var adicionais = _adicionalRepositorio.Obter();
+            return Ok(adicionais);
+        }
+
+        [HttpGet]
+        [Route("{id : int}")]
+        public IHttpActionResult ObterPorId(int id)
+        {
+            var adicionais = _adicionalRepositorio.ObterPorId(id);
+            return Ok(adicionais);
+        }
+        
+    }
+}
