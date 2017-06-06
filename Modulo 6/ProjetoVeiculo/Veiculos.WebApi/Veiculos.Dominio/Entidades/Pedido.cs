62757c0ed57e2428ff66fd3356e5b591b923283a
camila.batista <camila.batista@cwi.com.br>
2017-06-06 14:09:42 -0300

    feat(projetoVeiculo): adicionar entidade Adicionais, Pacote, Pedido e Produto

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veiculos.Infraestrutura.Entidades
+{
+    public class Pedido
+    {
+        public int Id { get; private set; }
+
+        public decimal ValorTotal { get; set; }
+
+        public DateTime DataInicial { get; set; }
+
+        public DateTime DataFinal { get; set; }
+
+        public Pedido() { }
+    }
+}
---
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.Dominio/Entidades/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veiculos.Infraestrutura.Entidades
+{
+    public class Produto
+    {
+        public int Id { get; private set; }
+
+        public string Nome { get; set; }
+
+        public decimal ValorUnitario { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public Produto() { }
+    }
+}
