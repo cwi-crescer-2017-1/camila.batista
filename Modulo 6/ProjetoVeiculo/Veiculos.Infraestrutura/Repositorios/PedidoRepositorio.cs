1e2385cad1ddcac20605d6f068470f59392dff53
Unknown <camila.batista@cwi.com.br>
2017-06-06 23:00:52 -0300

    feat(projetoVeiculo): adicionar pedido repositorio e controller

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Repositorios/PedidoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veiculos.Dominio.Entidades;
+
+namespace Veiculos.Infraestrutura.Repositorios
+{
+    public class PedidoRepositorio : BasicRepositorio<Pedido>
+    {
+        public PedidoRepositorio() { }
+    }
+}
---
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.WebApi/Controllers/PedidoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Veiculos.Dominio.Entidades;
+using Veiculos.Infraestrutura.Repositorios;
+
+namespace Veiculos.WebApi.Controllers
+{
+    public class PedidoController : ApiController
+    {
+        readonly PedidoRepositorio _pedidoRepositorio;
+
+        public PedidoController()
+        {
+            _pedidoRepositorio = new PedidoRepositorio();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Obter()
+        {
+            var pedido = _pedidoRepositorio.GetAll();
+            return Ok(pedido);
+        }
+
+        [HttpGet]
+        public IHttpActionResult ObterPorId(int id)
+        {
+            var pedido = _pedidoRepositorio.GetById(id);
+            return Ok(pedido);
+        }
+
+        [HttpPut]
+        public void Adicionar(Pedido pedido)
+        {
+            _pedidoRepositorio.Insert(pedido);
+        }
+
+        [HttpDelete]
+        public void Apagar(Pedido pedido)
+        {
+            _pedidoRepositorio.Delete(pedido);
+        }
+    }
+}
