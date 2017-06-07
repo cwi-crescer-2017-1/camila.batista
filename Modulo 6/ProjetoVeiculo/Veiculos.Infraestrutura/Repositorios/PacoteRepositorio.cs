2f47c81fd4564208375d66fa30706734bf23f63b
Unknown <camila.batista@cwi.com.br>
2017-06-06 22:57:19 -0300

    feat(projetoVeiculo): adicionar pacote repositorio e controller

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Repositorios/PacoteRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Veiculos.Dominio.Entidades;
+
+namespace Veiculos.Infraestrutura.Repositorios
+{
+    public class PacoteRepositorio : BasicRepositorio<Pacote>
+    {
+        public PacoteRepositorio() { }
+    }
+}
---
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.WebApi/Controllers/PacoteController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Veiculos.Dominio.Entidades;
+using Veiculos.Infraestrutura.Repositorios;
+
+namespace Veiculos.WebApi.Controllers
+{
+    public class PacoteController : ApiController
+    {
+        readonly PacoteRepositorio _pacoteRepositorio;
+
+        public PacoteController()
+        {
+            _pacoteRepositorio = new PacoteRepositorio();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Obter()
+        {
+            var pacotes = _pacoteRepositorio.GetAll();
+            return Ok(pacotes);
+        }
+
+        [HttpGet]
+        public IHttpActionResult ObterPorId(int id)
+        {
+            var pacotes = _pacoteRepositorio.GetById(id);
+            return Ok(pacotes);
+        }
+
+        [HttpPut]
+        public void Adicionar(Pacote pacote)
+        {
+            _pacoteRepositorio.Insert(pacote);
+        }
+
+        [HttpDelete]
+        public void Apagar(Pacote pacote)
+        {
+            _pacoteRepositorio.Delete(pacote);
+        }
+    }
+}
