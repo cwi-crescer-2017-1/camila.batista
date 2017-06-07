87b76bdecf59d7e467f563d3905df04d6bd120ab
Unknown <camila.batista@cwi.com.br>
2017-06-06 22:57:53 -0300

    feat(projetoVeiculo): adicionar produto repositorio e controller

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.WebApi/Controllers/ProdutoController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Veiculos.Dominio.Entidades;
+using Veiculos.Infraestrutura.Repositorios;
+
+namespace Veiculos.WebApi.Controllers
+{
+    public class ProdutoController : ApiController
+    {
+        readonly ProdutoRepositorio _produtoRepositorio;
+
+        public ProdutoController()
+        {
+            _produtoRepositorio = new ProdutoRepositorio();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Obter()
+        {
+            var produtos = _produtoRepositorio.GetAll();
+            return Ok(produtos);
+        }
+
+        [HttpGet]
+        public IHttpActionResult ObterPorId(int id)
+        {
+            var produtos = _produtoRepositorio.GetById(id);
+            return Ok(produtos);
+        }
+
+        [HttpPut]
+        public void Adicionar(Produto produto)
+        {
+            _produtoRepositorio.Insert(produto);
+        }
+
+        [HttpDelete]
+        public void Apagar(Produto produto)
+        {
+            _produtoRepositorio.Delete(produto);
+        }
+    }
+}
