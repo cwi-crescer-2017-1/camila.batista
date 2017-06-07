5eb3ff8f80c79f283a7adea8c9dc5d535f172f13
camila.batista <camila.batista@cwi.com.br>
2017-06-07 20:03:10 -0300

    feat(projetoVeiculo): adicionar validação da senha

--- Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.Dominio/Entidades/Funcionario.cs
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.Dominio/Entidades/Funcionario.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Veiculos.Dominio.Enum;
@@ -19,6 +20,10 @@ namespace Veiculos.Dominio.Entidades
 
         public string Senha { get; set; }
 
+        static readonly char[] _caracteresNovaSenha = "abcdefghijklmnopqrstuvzwyz1234567890*-_".ToCharArray();
+
+        static readonly int _numeroCaracteresNovaSenha = 10;
+
         public Funcionario() { }
 
         public Funcionario(string nome, Tipo cargo, string login, string senha)
@@ -27,15 +32,6 @@ namespace Veiculos.Dominio.Entidades
             Cargo = cargo;
             Login = login;
             Senha = senha;
-
-            if (Cargo.Equals(1))
-            {
-                cargo = Tipo.ADMINISTRADOR;
-            }
-            else if (Cargo.Equals(2))
-            {
-                cargo = Tipo.OPERADOR;
-            }
         }
 
         List<string> mensagens = new List<string>();
@@ -55,6 +51,36 @@ namespace Veiculos.Dominio.Entidades
             }
 
             return mensagens.Count() > 0 ? true : false;
+        }
+
+        public string ResetarSenha()
+        {
+            var senha = string.Empty;
+            for (int i = 0; i < Senha.Length; i++)
+            {
+                senha += new Random().Next(0, _caracteresNovaSenha.Length);
+            }
+
+            Senha = CriptografarSenha(senha);
+
+            return senha;
+        }
+
+        private string CriptografarSenha(string senha)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] inputBytes = Encoding.Default.GetBytes(Login + senha);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+                sb.Append(hash[i].ToString("x2"));
+
+            return sb.ToString();
+        }
+
+        public bool ValidarSenha(string senha)
+        {
+            return CriptografarSenha(senha) == Senha;
         }
     }
 }
