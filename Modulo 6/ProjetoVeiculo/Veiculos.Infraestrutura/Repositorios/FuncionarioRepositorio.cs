220a5b36a2c4ba1098b1bb404a64ae0e91af13eb
camila.batista <camila.batista@cwi.com.br>
2017-06-07 17:20:23 -0300

    feat(projetoVeiculo): adicionar getLogin

--- Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Repositorios/FuncionarioRepositorio.cs
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Repositorios/FuncionarioRepositorio.cs
@@ -10,5 +10,12 @@ namespace Veiculos.Infraestrutura.Repositorios
     public class FuncionarioRepositorio : BasicRepositorio<Funcionario>
     {
         public FuncionarioRepositorio() { }
+
+        private readonly Contexto _dbContext = new Contexto();
+
+        public Funcionario GetLogin(string login)
+        {
+            return _dbContext.Funcionarios.FirstOrDefault(x => x.Login == login);
+        }
     }
 }
