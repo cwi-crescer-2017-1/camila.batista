643aa8d08317e13c8416afa92c50e22a6565c7f9
Unknown <camila.batista@cwi.com.br>
2017-06-06 23:01:58 -0300

    feat(projetoVeiculo): adicionar cliente view model

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.WebApi/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Veiculos.Dominio.Enum;
+
+namespace Veiculos.WebApi.ViewModels
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; private set; }
+
+        public string Nome { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string CPF { get; set; }
+
+        public Genero Sexo { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+    }
+}
