5c5ba2de21eadcefd5999829fa643068bba79082
Unknown <camila.batista@cwi.com.br>
2017-06-06 22:55:37 -0300

    feat(projetoVeiculo): adicionar basicRepositorio

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Repositorios/BasicRepositorio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veiculos.Infraestrutura.Repositorios
+{
+    public class BasicRepositorio<TEntity> where TEntity : class
+    {
+        private readonly Contexto _dbContext = new Contexto();
+
+        public BasicRepositorio()
+        {
+        }
+
+        public void Insert(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Add(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _dbContext.Set<TEntity>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public IQueryable<TEntity> SearchFor(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbContext.Set<TEntity>().Where(predicate);
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _dbContext.Set<TEntity>();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _dbContext.Set<TEntity>().Find(id);
+        }
+    }
+}
