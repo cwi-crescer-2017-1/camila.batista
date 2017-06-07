448e4d1dc38b3213e2263e59a9bd12e2bb599495
Unknown <camila.batista@cwi.com.br>
2017-06-06 22:56:59 -0300

    feat(projetoVeiculo): adicionar funcionario repositorio e controller

---
+++ Modulo 6/ProjetoVeiculo/Veiculos.WebApi/Veiculos.WebApi/Controllers/FuncionarioController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Veiculos.Dominio.Entidades;
+using Veiculos.Infraestrutura.Repositorios;
+
+namespace Veiculos.WebApi.Controllers
+{
+    public class FuncionarioController : ApiController
+    {
+        readonly FuncionarioRepositorio _funcionarioRepositorio;
+
+        public FuncionarioController()
+        {
+            _funcionarioRepositorio = new FuncionarioRepositorio();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Obter()
+        {
+            var funcionarios = _funcionarioRepositorio.GetAll();
+            return Ok(funcionarios);
+        }
+
+        [HttpGet]
+        public IHttpActionResult ObterPorId(int id)
+        {
+            var funcionarios = _funcionarioRepositorio.GetById(id);
+            return Ok(funcionarios);
+        }
+
+        [HttpPut]
+        public void Adicionar(Funcionario funcionario)
+        {
+            _funcionarioRepositorio.Insert(funcionario);
+        }
+
+        [HttpDelete]
+        public void Apagar(Funcionario funcionario)
+        {
+            _funcionarioRepositorio.Delete(funcionario);
+        }
+    }
+}
