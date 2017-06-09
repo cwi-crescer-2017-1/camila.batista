303acfeee275375925b462e6c44607b9831d57d2
camila.batista <camila.batista@cwi.com.br>
2017-06-09 13:31:29 -0300

    feat(projetoVeiculo): adicionar metodo para relatorio mensal

--- Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Repositorios/PedidoRepositorio.cs
+++ Modulo 6/ProjetoVeiculo/Veiculos.Infraestrutura/Repositorios/PedidoRepositorio.cs
@@ -10,5 +10,20 @@ namespace Veiculos.Infraestrutura.Repositorios
     public class PedidoRepositorio : BasicRepositorio<Pedido>
     {
         public PedidoRepositorio() { }
+
+        public List<Pedido> obetRelatorioMensal(int mes)
+        {
+            return _dbContext.Set<Pedido>().Include("adicionais").Include("funcionario").Include("cliente").Include("produto").Include("pacote").Where(x => x.DataInicial.Month == mes).ToList(); 
+        }
+
+        //public override List<Pedido> GetAll()
+        //{
+        //    return _dbContext.Set<Pedido>().Include("adicionais").Include("funcionario").Include("cliente").Include("produto").Include("pacote").ToList();
+        //}
+
+        //public List<Pedido> obter()
+        //{
+        //    return _dbContext.Set<Pedido>().Include("adicionais").Include("funcionario").Include("cliente").Include("produto").Include("pacote").ToList();
+        //}
     }
 }
