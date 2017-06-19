5cd0410fba9c6d665381e56ddb69fb835ee1786f
Unknown <camila.batista@cwi.com.br>
2017-06-18 23:54:09 -0300

    feat(modulo7): adicionar exercício B

--- Modulo 7/MEGASENA.sql
+++ Modulo 7/MEGASENA.sql
@@ -102,6 +102,94 @@ CREATE OR REPLACE PACKAGE BODY PCK_MEGASENA AS
 
   -------------------------------------- B ------------------------------------------------------------------------------------------------
 
-    
-END PCK_MEGASENA;
+    FUNCTION premio (idConcurso IN NUMERIC, tipo IN NUMERIC, ganhadores IN NUMERIC) RETURN CONCURSO.PREMIO_QUINA$TYPE AS 
+        fReturn CONCURSO.PREMIO_QUINA$TYPE;
         
+        BEGIN
+            IF(tipo = 1) THEN
+                SELECT PREMIO_SENA INTO fReturn FROM CONCURSO WHERE IDCONCURSO = idConcurso;
+            ELSIF(tipo = 2) THEN
+                SELECT PREMIO_QUINA INTO fReturn FROM CONCURSO WHERE IDCONCURSO = idConcurso;
+            ELSIF (tipo = 3) THEN
+                SELECT PREMIO_QUADRA INTO fReturn FROM CONCURSO WHERE IDCONCURSO = idConcurso;
+            END IF;
+            
+        fReturn := fReturn / ganhadores;
+        RETURN fReturn;
+    END premio;
+
+    PROCEDURE atualizaAcertadores(pConcurso IN INTEGER) AS
+        
+        CURSOR ganhadores IS
+            SELECT * FROM 
+                (
+                  SELECT A.IDAPOSTA, 
+                    COUNT(CASE WHEN 
+                        N.NUMERO = C.PRIMEIRA_DEZENA OR
+                        N.NUMERO = C.SEGUNDA_DEZENA OR
+                        N.NUMERO = C.TERCEIRA_DEZENA OR
+                        N.NUMERO = C.QUARTA_DEZENA OR
+                        N.NUMERO = C.QUINTA_DEZENA OR
+                        N.NUMERO = C.SEXTA_DEZENA
+                    THEN 1 END) AS ACERTOS
+                  FROM APOSTAS A
+                    INNER JOIN CONCURSO C ON C.IDCONCURSO = A.IDCONCURSO
+                    INNER JOIN NUMERO_APOSTA N ON C.IDAPOSTA = N.IDAPOSTA
+                    WHERE C.IDCONCURSO = pConcurso
+                    GROUP BY A.IDAPOSTA  
+                )
+            WHERE ACERTOS > 3;
+        
+        vGanhadoresQuadra INTEGER := 0;
+        vGanhadoresQuina INTEGER := 0;
+        vGanhadoresSena INTEGER := 0;
+        vPremioQuadra CONCURSO.PREMIO_QUADRA%TYPE;
+        vPremioQuina CONCURSO.PREMIO_QUINA%TYPE;
+        vPremioSena CONCURSO.PREMIO111_SENA%TYPE;
+        vIdConcurso CONCURSO.IDCONCURSO%TYPE := pConcurso;
+        vAcumulou CONCURSO.ACUMULOU%TYPE;
+        vValorGanhador APOSTA_PREMIADA.VALOR%TYPE;
+            
+        BEGIN
+           FOR G IN ganhador LOOP
+            IF (G.ACERTOS = 4) THEN
+                vGanhadoresQuadra := vGanhadoresQuadra + 1;
+            ELSIF (G.ACERTOS = 5) THEN
+                vGanhadoresQuina := vGanhadoresQuina + 1;
+            ELSIF (G.ACERTOS = 6) THEN
+                vGanhadoresSena := vGanhadoresSena + 1;
+            END IF;
+           END LOOP;
+           
+        
+        IF(vGanhadoresQuadra > 0) THEN
+            vPremioQuadra := premio(vIdConcurso, 3, vGanhadoresQuadra);
+        ELSIF (vGanhadoresQuina > 0) THEN
+            vPremioQuina := premio(vIdConcurso, 2, vGanhadoresQuina);
+        ELSIF(vGanhadoresSena > 0) THEN
+            vPremioSena := premio(vIdConcurso, 1, vGanhadoresSena);
+        END IF;
+        
+
+        IF(vAcumulou = 1) THEN
+            UPDATE CONCURSO SET Acumulou = 1 WHERE IDCONCURSO = vIdConcurso;
+        END IF;
+        
+        FOR G IN ganhadores LOOP
+            IF(G.ACERTOS = 4) THEN
+                vValorGanhador := vPremioQuadra;
+            ELSIF(G.ACERTOS = 5) THEN
+                vValorGanhador := vPremioQuina;
+            ELSIF(G.ACERTOS = 6) THEN
+                vValorGanhador := vPremioSena;
+            END IF;
+        
+            INSERT INTO APOSTA_PREMIADA (IDAPOSTA_PREMIADA, IDAPOSTA, ACERTOS, VALOR)
+            VALUES(IDAPOSTA_PREMIADA.NEXTVAL, G.IDAPOSTA, G.ACERTOS, G.vValorGanhador);    
+        END LOOP;
+        
+        END atualizaAcertadores;
+        BEGIN NULL;
+    
+END PCK_MEGASENA; 
+
