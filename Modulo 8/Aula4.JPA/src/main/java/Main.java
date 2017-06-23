b0c69e3787a5427ef3d4d8551445f7ba7a4bad67
camila.batista <camila.batista@cwi.com.br>
2017-06-23 17:38:41 -0300

    feat(aula4): adicionar exemplos de hibernate

--- Modulo 8/Aula4.JPA/src/main/java/Main.java
+++ Modulo 8/Aula4.JPA/src/main/java/Main.java
@@ -3,6 +3,7 @@ import Pojo.Cliente;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import org.hibernate.Session;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -16,23 +17,39 @@ import javax.persistence.Persistence;
  */
 public class Main {
     public static void main(String[] args) {
-        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("MEGASENA");
-	final EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        
+//  JPA        
+//        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("MEGASENA");
+//	final EntityManager em = emf.createEntityManager();
+//        em.getTransaction().begin();
+//        
         
 //        Cliente c1 = new Cliente();
 //        c1.setNome("Pudim");
 //        c1.setId((long)1);
-        Cliente c1 = em.find(Cliente.class, 1L);
 
-        System.out.println(c1.getNome());
+//        Cliente c1 = em.find(Cliente.class, 1L);
+//        System.out.println(c1.getNome());
+  
+//        Cliente c1 = em.find(Cliente.class, 1L);
+//        em.remove(c1);
+//
+//        em.persist(c1);
+//        
+//        
+//        em.getTransaction().commit();
+//        em.close();
+//        emf.close();
+    
+    
+    //HIBERNATE
+    
+        final EntityManagerFactory emf = Persistence.createEntityManagerFactory("MEGASENA");
+	final EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
         
-        em.persist(c1);
+
+        final Session session = em.unwrap(Session.class);
         
-        
-        em.getTransaction().commit();
-        em.close();
-        emf.close();
+        final Cliente cliente = (Cliente) session.load(Cliente, args);
     }
 }
