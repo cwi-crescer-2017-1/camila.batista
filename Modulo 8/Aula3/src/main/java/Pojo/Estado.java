44271c45ef04f53395e0c9dd99821030f2e3835f
Unknown <camila.batista@cwi.com.br>
2017-06-23 09:27:59 -0300

    feat(aula3): adicionar classes pojo Estado, Pais e Pessoa

---
+++ Modulo 8/Aula3/src/main/java/Pojo/Estado.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pojo;
+
+/**
+ *
+ * @author camila.batista
+ */
+public class Estado {
+    private Long id;
+    private String nome;
+    private String uf;
+    private Long idPais;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Long getIdPais() {
+        return idPais;
+    }
+
+    public void setIdPais(Long idPais) {
+        this.idPais = idPais;
+    }
+
+
+    
+    
+}
--- Modulo 8/Aula3/src/main/java/Pojo/Run.java
+++ Modulo 8/Aula3/src/main/java/Pojo/Run.java
@@ -1,4 +1,4 @@
-package br.com.crescer.aula3;
+package Pojo;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
