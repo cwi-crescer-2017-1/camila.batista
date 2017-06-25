ad6ca0064562afb1ebdc18c1b78d5b5812e50091
Unknown <camila.batista@cwi.com.br>
2017-06-25 08:57:46 -0300

    feat(aula4): mapeamento da tabela cliente

--- Modulo 8/Aula4.JPA/src/main/java/Pojo/Cliente.java
+++ Modulo 8/Aula4.JPA/src/main/java/Pojo/Cliente.java
@@ -18,19 +18,58 @@ public class Cliente implements Serializable{
 
     @Id
     @Basic(optional=false)
-    @Column(name="ID_CLIENTE")
+    @SequenceGenerator(name = "SEQ_CLIENTE", sequenceName = "SEQ_CLIENTE")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="SEQ_CLIENTE")    
     private long id;
     
-    @Basic(optional = false)
-    @Column(name = "NM_CLIENTE")
+    @Column(name = "NOME")
     private String nome;
+    
+    @Column(name = "CPF")
+    private String CPF;
+    
+    @Column(name = "RG")
+    private String RG;
+    
+    @Column(name = "RUA")
+    private String rua;
+    
+    @Column(name = "BAIRRO")
+    private String bairro;
+    
+    @Column(name = "CIDADE")
+    private String cidade;
+    
+    @Column(name = "NUMERO_CASA")
+    private String numeroCasa;
+    
+    @Column(name = "EMAIL")
+    private String email;
+    
+    @Column(name = "TELEFONE")
+    private String telefone;
+    
+    @Column(name = "CELULAR")
+    private String celular;
+    
+    @Column(name = "DATA_NASCIMENTO")
+    private Date dataNascimento;
     
     public Cliente(){}
 
-    public Cliente(String nome) {
+    public Cliente(String nome, String CPF, String RG, String rua, String bairro, String cidade, String numeroCasa, String email, String telefone, String celular, Date dataNascimento) {
         this.nome = nome;
+        this.CPF = CPF;
+        this.RG = RG;
+        this.rua = rua;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.numeroCasa = numeroCasa;
+        this.email = email;
+        this.telefone = telefone;
+        this.celular = celular;
+        this.dataNascimento = dataNascimento;
     }
-    
     
     public long getId() {
         return id;
@@ -46,4 +85,86 @@ public class Cliente implements Serializable{
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(String numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    
 }
