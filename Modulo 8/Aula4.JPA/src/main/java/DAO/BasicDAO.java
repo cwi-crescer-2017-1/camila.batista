6963381c80925199143084d1640a0f14bfcd22f8
Unknown <camila.batista@cwi.com.br>
2017-06-25 09:16:24 -0300

    feat(aula4): adicionar DAO basico

---
+++ Modulo 8/Aula4.JPA/src/main/java/DAO/BasicDAO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author Camila
+ */
+public class BasicDAO<T, ID> implements CrudDAO<T, ID>{
+    
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("CRESCER");
+    private final EntityManager em = emf.createEntityManager();
+
+    private final Session session;
+    private Class<T> entity;
+
+    
+    public GenericDAO(Class<T> entity){
+        this.entity = entity;
+        em.getTransaction().begin();
+        this.session = em.unwrap(Session.class);
+    }
+    
+    @Override
+    public T save(T e){
+        session.saveOrUpdate(e);
+        session.getTransaction().begin();
+        this.session = em.unwrap(Session.class);
+    }
+    
+    @Override
+    public void remove(T e){
+        session.delete(e);
+        session.getTransaction().commit();
+    }
+    
+    @Override
+    public T loadById(ID id){
+        return em.find(entity, id);
+    }
+    
+    @Override
+    public List<T> findAll(){
+        return session.createCriteria(entity).list();
+    }
+    
+    public void close(){
+        session.close();
+        em.close();
+        emf.close();
+    }
+}
---
+++ Modulo 8/Aula4.JPA/src/main/java/DAO/CrudDao.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author Camila
+ */
+public interface CrudDao<Entity, ID> {
+    
+    Entity save(Entity e);
+
+    void remove(Entity e);
+
+    Entity loadById(ID id);
+
+    List<Entity> findAll();
+    
+}
