551e60a7b493836e105e5df9c9364fe235d4735d
Unknown <camila.batista@cwi.com.br>
2017-06-25 08:59:58 -0300

    feat(aula4): mapeamento da tabela funcionario

---
+++ Modulo 8/Aula4.JPA/src/main/java/Pojo/Funcionario.java
@@ -0,0 +1,176 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pojo;
+
+/**
+ *
+ * @author Camila
+ */
+@Entity
+@Table(name = "FUNCIONARIO")
+public class Funcionario {
+    
+    @Id
+    @SequenceGenerator(name = "SEQ_FUNCIONARIO", sequenceName = "SEQ_FUNCIONARIO")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_FUNCIONARIO")
+    private Long id;
+    
+    @Column(name = "NOME");
+    private String nome;
+    
+    @Column(name = "BAIRRO");
+    private String bairro;
+    
+    @Column(name = "CIDADE");
+    private String cidade;
+    
+    @Column(name = "NUMERO_CASA");
+    private String numeroCasa;
+    
+    @Column(name = "RUA");
+    private String rua;
+    
+    @Column(name = "RG");
+    private String RG;
+    
+    @Column(name = "EMAIL");
+    private String email;
+    
+    @Column(name = "TELEFONE");
+    private String telefone;
+    
+    @Column(name = "CELULAR");
+    private String celular;
+    
+    @Column(name = "SALARIO");
+    private double salario;
+    
+    @Column(name = "FUNCAO");
+    private String funcao;
+
+    @Column(name = "CPF");
+    private String CPF;
+    
+    @Column(name = "DATA_NASCIMENTO");
+    private Date dataNascimento;
+    
+    public Funcionario() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(String numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getFuncao() {
+        return funcao;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+    
+}
