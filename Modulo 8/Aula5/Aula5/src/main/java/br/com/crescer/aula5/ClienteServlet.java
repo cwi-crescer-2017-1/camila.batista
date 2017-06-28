74c0a428b53a1a3425231f1f2f5015b02fa1aa1e
camila.batista <camila.batista@cwi.com.br>
2017-06-28 17:33:12 -0300

    feat(): adicionar exemplos das aulas 5 e 6

---
+++ Modulo 8/Aula5/Aula5/src/main/java/br/com/crescer/aula5/ClienteServlet.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula5;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author camila.batista
+ */
+public class ClienteServlet extends HttpServlet{
+
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+//        try(final PrintWriter out = resp.getWriter()){
+//            out.append("Camila");
+//        }
+//    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        try (final PrintWriter out = response.getWriter();) {
+            out.append("<!DOCTYPE html>");
+            out.append("<html>");
+            out.append("<head>");
+            out.append("<title>Java - aula5</title>");
+            out.append("<meta charset=\"UTF-8\">");
+            out.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            out.append("</head>");
+            out.append("<body>");
+            out.append("<h1>Cliente</h1>");
+            out.append("</body>");
+            out.append("</html>");
+        }
+    }
+}
