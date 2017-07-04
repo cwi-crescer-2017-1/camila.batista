121972e6b1a21f3d42d3dc0da725b33a1d6ac37c
Unknown <camila.batista@cwi.com.br>
2017-07-04 08:59:41 -0300

    feat(redeSocial): adicionar metodos relacionados aos amigos

--- Modulo 8/RedeSocial/RedeSocial/src/main/java/Controller/UsuarioController.java
+++ Modulo 8/RedeSocial/RedeSocial/src/main/java/Controller/UsuarioController.java
@@ -8,8 +8,15 @@ package Controller;
 import Entidades.Usuario;
 import Service.UsuarioService;
 import java.util.List;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,13 +33,12 @@ public class UsuarioController {
     UsuarioService service;
     
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String list(){
-        return "teste";
-        //return service.list();
+    public List<Usuario> list(){
+        return service.list();
     }
     
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Usuario findById(Long id){
+    public Usuario findById(@PathVariable Long id){
         return service.findById(id);
     }
     
@@ -50,6 +56,15 @@ public class UsuarioController {
         }
     }
     
+    @GetMapping(value = "/amigos")
+    public Set<Usuario> getAmigos(@AuthenticationPrincipal User user) throws Exception{
+        return (Set<Usuario>) service.findByEmail(user.getUsername()).getAmigos();
+    }
+    
+    @GetMapping(value = "/convites")
+    public Set<Usuario> getConvites(@AuthenticationPrincipal User user) throws Exception{
+        return (Set<Usuario>) service.findByEmail(user.getUsername()).getConvites();
+    }
             
     @RequestMapping(value = "", method = RequestMethod.POST)
     public Usuario save(Usuario usuario) throws Exception{
@@ -60,4 +75,29 @@ public class UsuarioController {
     public void delete(Long id){
         service.delete(id);
     }
+    
+    @PostMapping(value = "/update")
+    public Usuario update(@RequestBody Usuario usuario, @AuthenticationPrincipal User user) throws Exception{
+        Usuario us1 = service.findByEmail(user.getUsername());
+        us1.setNome(usuario.getNome());
+        us1.setSenha(usuario.getSenha());
+        return service.update(us1);
+    }
+    
+    @PostMapping(value = "/convite/aceitar/{id}")
+    public void enviarConvite(@PathVariable Long id, @AuthenticationPrincipal User user) throws Exception{
+        Usuario us1 = service.findByEmail(user.getUsername());
+        Usuario us2 = service.findById(id);
+        service.enviarConvite(us1, us2);
+    }
+    
+    @PostMapping(value = "/convite/rejeitar/{id}")
+    public void rejeitarConvite(@PathVariable Long id, @AuthenticationPrincipal User user) throws Exception{
+        Usuario us1 = service.findByEmail(user.getUsername());
+        Usuario us2 = service.findById(id);
+        service.rejeitarConvite(us1, us2);
+    }
+    
+    
+    
 }
